8ac40979a71f07073cccb721109e311f10e24f66
Omrani Salim <omrani_salim@outlook.fr>
2025-10-31 14:44:24 +0100

    Refactor imports in users.component.ts

--- src/app/features/users/users.component.ts
+++ src/app/features/users/users.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject, input, Input, OnInit, signal } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { UsersStore, User } from '../../store';
