633cd0fc6d08d488ad7cd65a12e04772dc387913
Omrani Salim <omrani_salim@outlook.fr>
2025-11-03 21:08:56 +0100

    remove localstore

--- src/app/interceptors/auth.interceptor.ts
+++ src/app/interceptors/auth.interceptor.ts
@@ -4,7 +4,7 @@ import { AuthStore } from '../store/auth.store';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
   const authStore = inject(AuthStore);
-  const token = authStore.accessToken() || (typeof localStorage !== 'undefined' ? localStorage.getItem('accessToken') : null);
+  const token = authStore.accessToken();
 
   if (token) {
     req = req.clone({ setHeaders: { Authorization: `Bearer ${token}` } });
